52745ba69cea3439544af7226a42267a8ee09d53
jwaresoft <jwaresoft@proton.me>
2025-03-14 14:36:42 -0700

    minor cleanup in WIP wordlist

--- src/components/userWordList/UserWordList.tsx
+++ src/components/userWordList/UserWordList.tsx
@@ -1,28 +1,40 @@
 import { IndexedAndLabeledPOSItem } from "@/types/indexedAndLabeledPOSItem";
 import "./userWordList.css";
 
-function UserWordList({ wordList }: { wordList: Array<IndexedAndLabeledPOSItem> }) {
-  console.log(wordList);
+/**
+ *
+ * Fill in the blanks component.  Presents inputs for a list of parts of speech, which
+ *
+ * @param wordlist - an array of IndexedAndLabeledPOSItems
+ * @returns Component
+ */
+function UserWordList({
+  wordList,
+}: {
+  wordList: Array<IndexedAndLabeledPOSItem>;
+}) {
   return (
     <div className="user-word-list-main">
-      <button>Clear</button>
-      {wordList.map((element, index) => {
-        const keyIndex = `user-input-${index}`;
+      <form>
+        <button>Clear</button>
+        {wordList.map((element: IndexedAndLabeledPOSItem, index: number) => {
+          const keyIndex = `user-input-${index}`;
 
-        return (
-          <>
-            <label htmlFor={keyIndex}>
-              {element.friendlyPOS ? element.friendlyPOS.toUpperCase() : ""}
-            </label>
-            <input
-              className="userPOSInput"
-              key={keyIndex}
-              id={keyIndex}
-            ></input>
-          </>
-        );
-      })}
-      <button>Submit</button>
+          return (
+            <>
+              <label htmlFor={keyIndex}>
+                {element.friendlyPOS ? element.friendlyPOS.toUpperCase() : ""}
+              </label>
+              <input
+                className="userPOSInput"
+                key={keyIndex}
+                id={keyIndex}
+              ></input>
+            </>
+          );
+        })}
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
