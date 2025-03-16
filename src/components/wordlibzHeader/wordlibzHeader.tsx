09a485538e4f35dd0015d6dabef64fc2e6fb4944
jwaresoft <jwaresoft@proton.me>
2025-03-16 16:14:00 -0700

    wip: add headerbutton & button, needs story + test

--- src/components/wordlibzHeader/wordlibzHeader.tsx
+++ src/components/wordlibzHeader/wordlibzHeader.tsx
@@ -1,17 +1,25 @@
-import "./wordlibzHeader.css";
+import { HeaderButton } from "@/components/buttons/buttons";
 import wordlibzLogo from "@/assets/wordlibz.svg";
+import "./wordlibzHeader.css";
 
 export function WordlibzHeader() {
   return (
     <div className="wordlibz-header">
       <div className="wordlibz-header-panel">
         <span>
-          <img src={wordlibzLogo} width={32}></img>
+          <img src={wordlibzLogo} height={32}></img>
         </span>
         <span>Wordlibz</span>
       </div>
       <div className="wordlibz-header-panel">
-        <button>ToDo</button>
+        <HeaderButton
+          label={"CLEAR"}
+          onClickHandler={() => console.log("ok")}
+        />
+        <HeaderButton
+          label={"NEW"}
+          onClickHandler={() => console.log("ok")}
+        />
       </div>
     </div>
   );
